38f4d1b563ef77d4d70396922416d5ee210501bc
Andy Pennebaker <andy.pennebaker@wicourts.gov>
2014-08-14 14:01:22 -0500

    rspec

--- features/step_definitions/steps.rb
+++ features/step_definitions/steps.rb
@@ -5,30 +5,30 @@ end
 Then(/^the output is correct for each test$/) do
   lines = @cucumber.split("\n")
 
-  lines.length.should == 19
+  expect(lines.length).to eq(19)
 
-  lines[0 .. 4].join("\n").should == 'diff --git a/examples/license-old.md b/examples/license-new.md
+  expect(lines[0 .. 4].join("\n")).to eq('diff --git a/examples/license-old.md b/examples/license-new.md
 index a9ac9dd..5c3f186 100644
 --- a/examples/license-old.md
 +++ b/examples/license-new.md
-@@ -1,11 +1,10 @@'
+@@ -1,11 +1,10 @@')
 
-  lines[5 .. 6].join("\n").should == ' # FreeBSD License
- '
+  expect(lines[5 .. 6].join("\n")).to eq(' # FreeBSD License
+ ')
 
-  lines[7 .. 9].join("\n").should == '-# Copyright 2012 Andrew Pennebaker. All rights reserved.
+  expect(lines[7 .. 9].join("\n")).to eq('-# Copyright 2012 Andrew Pennebaker. All rights reserved.
 +# Copyright 2012 YelloSoft. All rights reserved.
- '
+ ')
 
-  lines[10 .. 11].join("\n").should == ' Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
- '
+  expect(lines[10 .. 11].join("\n")).to eq(' Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+ ')
 
-  lines[12 .. 15].join("\n").should == ' 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+  expect(lines[12 .. 15].join("\n")).to eq(' 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
 -2. Redistributions in binary form must reproduce the copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
 +2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
- '
+ ')
 
-  lines[16 .. -1].join("\n").should == '-THIS SOFTWARE IS PROVIDED BY THE AUTHORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHORS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+  expect(lines[16 .. -1].join("\n")).to eq('-THIS SOFTWARE IS PROVIDED BY THE AUTHORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHORS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
 -LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-+THIS SOFTWARE IS PROVIDED BY THE AUTHORS "AS IS" AND ANY EXPRESS WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHORS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.'
++THIS SOFTWARE IS PROVIDED BY THE AUTHORS "AS IS" AND ANY EXPRESS WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHORS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.')
 end
